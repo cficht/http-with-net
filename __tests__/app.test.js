15a5dfb471f863837b7e76d5a56f0c9245e43fb4
cficht <chris.ficht@gmail.com>
2020-03-16 16:36:43 -0700

    created and tested GET / route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,6 +1,12 @@
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('createResponse', () => {
-
+describe('app routes', () => {
+  it('retrieves the home page', () => {
+    return request(app)
+      .get('/')
+      .then(res => {
+        expect(res.text).toEqual('hi');
+      });
+  });
 });
